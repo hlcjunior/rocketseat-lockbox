ffb4ec86dbf1a8e83c819af8b871054ec264dc31
hlcjunior <62648013+hlcjunior@users.noreply.github.com>
2025-02-07 16:31:28 -0300

    Removido

--- public/index.php
+++ public/index.php
@@ -1,13 +1,13 @@
 <?php
 
-require "../Core/functions.php";
+require "../vendor/autoload.php";
 
 //require '../App/Models/Usuario.php';
 
-spl_autoload_register(function ($class) {
-    $class = str_replace("\\", DIRECTORY_SEPARATOR, $class);
-    require base_path("$class.php");
-});
+//spl_autoload_register(function ($class) {
+//    $class = str_replace("\\", DIRECTORY_SEPARATOR, $class);
+//    require base_path("$class.php");
+//});
 
 session_start();
 
